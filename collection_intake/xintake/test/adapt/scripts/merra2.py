fb0f468674194bcd41e730e3dbfbe7e9bd26351b
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-20 18:29:14 -0500

    Update Collection

--- collection_intake/xintake/test/adapt/scripts/merra2.py
+++ collection_intake/xintake/test/adapt/scripts/merra2.py
@@ -6,9 +6,8 @@ import os
 collection_name = "cip_merra2_mon"
 collection_root = "/nfs4m/css/curated01/create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos"
 agg_dirs = glob( f"{collection_root}/*" )
-agg_dirs_test = [ agg_dirs[0] ]
 
-for agg_dir in agg_dirs_test:
+for agg_dir in agg_dirs:
     agg_name = os.path.basename( agg_dir )
     print( f"Creating aggregation {agg_name}")
     agg_files =  f"{agg_dir}/*.nc"
