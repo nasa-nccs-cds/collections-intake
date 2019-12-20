d75bb263559d5dadf5e0ba05bf1f6adadcaab8a8
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-12-20 14:26:11 -0500

    Add merra2 script

--- collection_intake/xintake/test/adapt/scripts/merra2.py
+++ collection_intake/xintake/test/adapt/scripts/merra2.py
@@ -6,6 +6,7 @@ import os
 collection_name = "cip_merra2_mon"
 collection_root = "/nfs4m/css/curated01/create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos/"
 agg_dirs = f"${collection_root}/*"
+print( f"Creating aggregations using glob: {agg_dirs}")
 
 for agg_dir in glob( agg_dirs ):
     agg_name = os.path.basename( agg_dir )
