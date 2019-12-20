c46184fe02c78b35ec4f43297a0a7cbccc7c198e
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-12-20 16:08:50 -0500

    Update merra2 script

--- collection_intake/xintake/aggregation.py
+++ collection_intake/xintake/aggregation.py
@@ -1,12 +1,9 @@
 import intake, os
 from intake_xarray.netcdf import NetCDFSource
 from intake.catalog.local import YAMLFileCatalog
+from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
 import xarray as xa
 from intake.source.base import DataSource
-
-def set_cat_attr( cat_source: DataSource, key: str, value: str ):
-    attr_value =  cat_source.metadata.get( value[1:], "") if value.startswith('@') else value
-    setattr(cat_source, key, attr_value )
 
 class Aggregation:
 
@@ -14,32 +11,49 @@ class Aggregation:
         self.name = name
         self.files = kwargs.get( "files", None )
         self.collection = kwargs.get( "collection", self.name )
+        self.dataSource: Optional[DataSource] = None
+        self.catalog: Optional[YAMLFileCatalog] = None
+        self.openDataSource( **kwargs )
 
-    def getCatalogFilePath(self, **kwargs):
+    def printMetadata(self, **kwargs):
+        self.openDataSource( **kwargs )
+        print( self.dataSource.metadata )
+
+    def getCatalogFilePath( self, **kwargs ):
         agg_dir = os.path.join( kwargs.get( "path", self.getCatalogsPath() ), self.collection, self.name )
         agg_catalog_file = os.path.join( agg_dir, "catalog.yaml")
         os.makedirs( agg_dir, exist_ok=True )
         return agg_catalog_file
 
-    def generate(self, **kwargs ):
-        cat_source: NetCDFSource = intake.open_netcdf( self.files, concat_dim="time" )
-        cat_source.discover()
-        attrs = kwargs.get("attrs",{})
-        for key,value in attrs.elems(): set_cat_attr( cat_source, key, value  )
-        cat_source.name = kwargs.get( 'name', self.name )
+    def openDataSource( self, **kwargs ):
+        if (self.dataSource is None) and kwargs.get( 'open', True ):
+            cdim = kwargs.get( "concat_dim", "time")
+            self.dataSource: NetCDFSource = intake.open_netcdf( self.files, concat_dim=cdim )
+            self.dataSource.discover()
+            attrs = kwargs.get("attrs",{})
+            for key,value in attrs.elems(): self.setSourceAttr( key, value  )
+            self.dataSource.name = kwargs.get( 'name', self.name )
+
+    def setSourceAttr( self, key: str, value: str):
+        attr_value = self.dataSource.metadata.get(value[1:], "") if value.startswith('@') else value
+        setattr( self.dataSource, key, attr_value)
+
+    def writeCatalogFile(self, **kwargs):
+        self.openDataSource( **kwargs )
         catalog_file = self.getCatalogFilePath( **kwargs )
 
         with open( catalog_file, 'w' ) as f:
-            yaml =  cat_source.yaml()
+            yaml =  self.dataSource.yaml()
             print( f"\nWriting aggregation {self.name} to {catalog_file}:\n\n{yaml}")
             f.write( yaml )
 
-    def open( self, **kwargs ) -> xa.Dataset:
-        cat_file = self.getCatalogFilePath( **kwargs )
-        print( f"Opening aggregation from file {cat_file}" )
-        data_source: YAMLFileCatalog = intake.open_catalog( cat_file, driver="yaml_file_cat")
-        data_source.discover()
-        ds: xa.Dataset = getattr( data_source, self.name ).to_dask()
+    def openFromCatalog(self, **kwargs) -> xa.Dataset:
+        if self.catalog is None:
+            cat_file = self.getCatalogFilePath( **kwargs )
+            print( f"Opening aggregation from file {cat_file}" )
+            self.catalog: YAMLFileCatalog = intake.open_catalog( cat_file, driver="yaml_file_cat")
+            self.catalog.discover()
+        ds: xa.Dataset = getattr( self.catalog, self.name ).to_dask()
         return ds
 
     def getCatalogsPath( self ):
@@ -47,11 +61,18 @@ class Aggregation:
         assert ilDataDir is not None, "Must set the ILDATA environment variable to define the data directory"
         return os.path.join(ilDataDir, "collections" )
 
+    def close(self):
+        if self.dataSource: self.dataSource.close()
+        if self.catalog:    self.catalog.close()
+
+    def __del__(self):
+        self.close()
+
 if __name__ == "__main__":
     files_glob = '/Users/tpmaxwel/Dropbox/Tom/Data/MERRA/MERRA2/6hr/*.nc4'
     agg = Aggregation( "merra2-6hr", collection="merra2", files=files_glob )
-    agg.generate()
+    agg.writeCatalogFile()
 
     agg1 =  Aggregation( "merra2-6hr", collection="merra2" )
-    ds = agg1.open( )
+    ds = agg1.openFromCatalog()
     print(f"variable QV: dims: {ds.QV.dims}, shape: {ds.QV.shape}, chunks = {ds.QV.chunks}")
--- collection_intake/xintake/test/adapt/scripts/merra2.py
+++ collection_intake/xintake/test/adapt/scripts/merra2.py
@@ -7,9 +7,13 @@ collection_name = "cip_merra2_mon"
 collection_root = "/nfs4m/css/curated01/create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos"
 agg_dirs = f"{collection_root}/*"
 print( f"Creating aggregations using glob: {agg_dirs}")
+agg_dirs_test = [ agg_dirs[0] ]
 
-for agg_dir in glob( agg_dirs ):
+for agg_dir in glob( agg_dirs_test ):
     agg_name = os.path.basename( agg_dir )
     print( f"Creating aggregation {agg_name}")
-#    agg = Aggregation( )
+    agg_files =  f"{agg_dir}/*.nc"
+    agg = Aggregation( agg_name, collection=collection_name, files=agg_files )
+    agg.printMetadata()
+#    agg.writeCatalogFile(attr = agg_attr)
 
