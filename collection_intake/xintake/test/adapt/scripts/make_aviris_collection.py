e0f60a7f82f0078699e270ee7625dfbded1f71c0
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-24 15:18:54 -0500

    Add read_aviris

---
+++ collection_intake/xintake/test/adapt/scripts/make_aviris_collection.py
@@ -0,0 +1,30 @@
+from glob import glob
+from collection_intake.xintake.aggregation import Aggregation
+from collection_intake.xintake.collection import Collection
+import os, intake
+print( f"Intake drivers: {list(intake.registry)}" )
+
+collection_name = "ORNL_ABoVE_Airborne_AVIRIS_NG"
+collection_root = "/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG/"
+aggs = { 'ang2017': f"{collection_root}/data" }
+catalog_files = []
+
+for agg_name, agg_dir in aggs.items():
+    print( f"Creating aggregation {agg_name}")
+    agg_files =  f"{agg_dir}/ang*rfl/ang*/*_img"
+
+    source: intake.DataSource = intake.open_rasterio(agg_files, concat_dim="time")
+    source.discover()
+    print( source.shape )
+
+#    agg = Aggregation( agg_name, collection=collection_name, files=agg_files )
+#    md = agg.getMetadata()
+#    agg.description = f"{md['description']}"
+#    catalog_files.append( agg.writeCatalogFile() )
+
+# collection = Collection( collection_name, description="ORNL ABoVE Airborne AVIRIS NG 2017" )
+# collection.generate( cats = catalog_files )
+#
+# root_collection = Collection( description="NCCS data collections" )
+# root_collection.generate(  )
+
