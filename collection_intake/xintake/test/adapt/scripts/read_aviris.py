a9c96f4b5004b3cb1a44a4c93a16185c1effb023
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-24 12:04:46 -0500

    Add read_aviris

--- collection_intake/xintake/test/adapt/scripts/read_aviris.py
+++ collection_intake/xintake/test/adapt/scripts/read_aviris.py
@@ -1,4 +1,6 @@
 import xarray as xa
 aviris_file = "/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG/data/ang20180819t010027/ang20180819t010027_rdn_v2r2/ang20180819t010027_rdn_v2r2_img"
-result = xa.open_rasterio( aviris_file )
-print( result.shape )
+result: xa.DataArray = xa.open_rasterio( aviris_file )
+print( f"shape = {result.shape}" )
+print( f"dims = {result.dims}" )
+print( f"attrs = {result.attrs}" )
