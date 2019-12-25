5a11417acefabe2a99d79e4a2f10bd00a556cf25
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-25 18:10:07 -0500

    Add make_aviris_collection

--- collection_intake/xintake/test/adapt/scripts/make_aviris_collection.py
+++ collection_intake/xintake/test/adapt/scripts/make_aviris_collection.py
@@ -2,6 +2,7 @@ from glob import glob
 from collection_intake.xintake.aggregation import Aggregation
 from collection_intake.xintake.collection import Collection
 from collection_intake.xintake.base import Grouping, pp
+from collections import OrderedDict
 from dateutil.parser import parse
 import os, intake
 print( f"Intake drivers: {list(intake.registry)}" )
@@ -11,7 +12,6 @@ def get_time( fname: str )-> str:
     time_end = time_start + 15
     timeval: str = fname[time_start:time_end]
     tval = parse( timeval.replace( "t", " " ) )
-    print( f"{timeval} ({tval}): {fname}")
     return timeval
 
 collection_name = "ORNL_ABoVE_Airborne_AVIRIS_NG"
@@ -19,13 +19,15 @@ collection_root = "/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_N
 aggs = dict( ang_rfl_v2r2 = f"{collection_root}/data/ang*/ang*_rdn_v2r2/*_img" )
 catalog_files = []
 
+ordered_files = OrderedDict()
 for agg_name, agg_files_glob in aggs.items():
     print( f"Creating aggregation {agg_name}")
     agg_files =  glob( agg_files_glob )
     print( "Got Aviris files: " )
-    agg_file_dict = { get_time(agg_file): agg_file for agg_file in agg_files }
+    agg_file_dict = OrderedDict( [ ( get_time(agg_file), agg_file ) for agg_file in agg_files ] )
+    print( agg_file_dict.keys() )
 
-    source: intake.DataSource = intake.open_rasterio(agg_files, chunks = {}, concat_dim="time")
+    source: intake.DataSource = intake.open_rasterio( agg_file_dict.values(), chunks = {}, concat_dim="time" )
     source.discover()
     print( source.shape )
 
