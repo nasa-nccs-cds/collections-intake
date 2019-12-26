145b2e2da81dab8d99a77a58e3d0d6fb0dadbdd6
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-26 12:45:53 -0500

    Add make_aviris_collection

--- collection_intake/xintake/test/adapt/scripts/make_aviris_collection.py
+++ collection_intake/xintake/test/adapt/scripts/make_aviris_collection.py
@@ -7,12 +7,12 @@ from dateutil.parser import parse
 import os, intake
 print( f"Intake drivers: {list(intake.registry)}" )
 
-def get_time( fname: str )-> str:
+def get_time( fname: str ):
     time_start = len(collection_root) + 9
     time_end = time_start + 15
     timeval: str = fname[time_start:time_end]
     tval = parse( timeval.replace( "t", " " ) )
-    return timeval
+    return tval
 
 collection_name = "ORNL_ABoVE_Airborne_AVIRIS_NG"
 collection_root = "/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG"
@@ -25,7 +25,7 @@ for agg_name, agg_files_glob in aggs.items():
     agg_files =  glob( agg_files_glob )
     print( "Got Aviris files: " )
     agg_file_dict = OrderedDict( [ ( get_time(agg_file), agg_file ) for agg_file in agg_files ] )
-    print( agg_file_dict.keys() )
+    pp( agg_file_dict.keys() )
 
     source: intake.DataSource = intake.open_rasterio( agg_file_dict.values(), chunks = {}, concat_dim="time" )
     source.discover()
