fe3203593308a1d566e495e409a3b080c9735adc
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-27 13:15:35 -0500

    Update collection paths

--- collection_intake/xintake/sources/aviris.py
+++ collection_intake/xintake/sources/aviris.py
@@ -2,6 +2,7 @@ from distutils.version import LooseVersion
 from intake.source.base import PatternMixin
 from intake.source.utils import reverse_format
 from intake_xarray.base import DataSourceMixin
+from collection_intake.xintake.base import Grouping, pp, str_dict
 from collection_intake.xintake.aggregation import Aggregation
 from intake.source.base import DataSource
 import intake
@@ -9,8 +10,9 @@ import intake
 class AvirisFileCatEntry(Aggregation):
     name = 'aviris'
 
-    def __init__(self, fileId: str, collection: str, filePath: str,  **kwargs):
-        super(AvirisFileCatEntry, self).__init__( name=fileId, collection=collection, files=[filePath], **kwargs )
+    def __init__(self, fileId: str, aggregation: str, collection: str,  filePath: str,  **kwargs):
+        self.fileId = fileId
+        super(AvirisFileCatEntry, self).__init__( name=aggregation, collection=collection, files=[filePath], **kwargs )
 
     def getDataSource(self, **kwargs ) -> DataSource:
         try:
@@ -21,3 +23,6 @@ class AvirisFileCatEntry(Aggregation):
             print( f"Error opening Aviris file (skipping): {self.files[0]}: {err}")
             return None
 
+    def getCatalogFilePath( self, **kwargs ):
+        cat_nodes = kwargs.get( "cat_nodes", [ self.collection, self.name ] )
+        return Grouping.getCatalogFilePath( self, cat_nodes, name=self.fileId, **kwargs )
