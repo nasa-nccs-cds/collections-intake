260e03d8e8940d8d4f3f90e392936121b7d041c0
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-29 18:23:22 -0500

    Update Grouping api

--- collection_intake/xintake/base.py
+++ collection_intake/xintake/base.py
@@ -2,7 +2,9 @@ import intake, os, pprint, warnings
 from intake.config import conf as iconf
 from intake.catalog.local import Catalog
 from intake.catalog.local import YAMLFileCatalog
-from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
+from intake.source.base import DataSource
+from glob import glob
+from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional, Union
 import xarray as xa
 pp = pprint.PrettyPrinter(depth=4).pprint
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -12,32 +14,37 @@ class Grouping:
 
     def __init__( self, path_nodes: List[str], **kwargs ):
         self._catalog: Optional[Catalog] = None
-        self.path_nodes = path_nodes
+        self._path_nodes: List[str] = path_nodes
+        self._files: List[str] = kwargs.get('files',None)
         self._driver = kwargs.get( "driver", "yaml_file_cat" )
+        self._source = kwargs.get("driver", "yaml_file_cat")
         self._catalog: Catalog = self.initCatalog(**kwargs)
 
     def printMetadata(self):
         pp( self._catalog.metadata )
 
     @property
+    def name(self) -> str:
+        return self._path_nodes[-1]
+
+    @property
     def catalog(self) -> Catalog:
         return self._catalog
 
-    def addSubGroup(self, pathNode: str, **kwargs ) -> "Grouping":
-        subGroup: Grouping =  Grouping( self.path_nodes + pathNode, **kwargs )
-        self._addToCatalog( subGroup )
+    def addSubGroup(self, name: str, **kwargs ) -> "Grouping":
+        subGroup: Grouping =  Grouping( self._path_nodes + name, **kwargs )
+        self._addToCatalog( subGroup.catalog )
         return subGroup
 
-    def _addToCatalog(self, subGroup: "Grouping" ):
+    def _addToCatalog(self, source: DataSource ):
         if self._driver == "yaml_file_cat":
             yaml_file_cat: YAMLFileCatalog = self._catalog
-            yaml_file_cat.add( subGroup.catalog )
+            yaml_file_cat.add( source )
         else:
             raise Exception( f"Unrecognized driver: {self._driver}")
 
-
     def initCatalog(self, **kwargs ) -> Catalog:
-        cat_uri = self.getCatalogURI(**kwargs)
+        cat_uri = self.getURI(**kwargs)
         if os.path.isfile( cat_uri ):
             catalog: Catalog = intake.open_catalog(cat_uri, driver=self._driver)
             catalog.discover()
@@ -46,37 +53,35 @@ class Grouping:
             catalog: Catalog = registry[self._driver]( cat_uri, **kwargs )
         description = kwargs.get( "description", None )
         metadata = kwargs.get( "metadata", None )
-        name = kwargs.get( "name", None )
         if description: catalog.description = description
         if metadata: catalog.metadata = metadata
-        if name: catalog.name = name
+        catalog.name = self.name
         catalog.save( cat_uri )
         return catalog
 
-    def getCatalogURI ( self, **kwargs ):
-        root_dir = kwargs.get( "base", self.getcatalogsPath() )
-        name = kwargs.get( "name", self.name )
-        cat_dir = os.path.join( root_dir, *self.path_nodes )
-        catalog_file = os.path.join( cat_dir, f"{name}.yaml" )
+    def getURI ( self, **kwargs ):
+        base_uri = kwargs.get( "base", self.getIntakeURI())
+        cat_dir = os.path.join( base_uri, *self._path_nodes )
+        catalog_uri = os.path.join( cat_dir, f"catalog.yaml" )
         os.makedirs( cat_dir, exist_ok=True )
-        return catalog_file
+        return catalog_uri
 
     @classmethod
-    def getcatalogsPath( cls ) -> str:
+    def getIntakeURI(cls) -> str:
         catalog_paths = iconf.get( "catalog_path"  )
         if catalog_paths is None:
             ilDataDir = os.environ.get('ILDATA')
             assert ilDataDir is not None, "Must set the ILDATA environment variable to define the data directory"
-            catalog_path = os.path.join( ilDataDir, "collections", "intake_IL" )
+            catalog_path = os.path.join( ilDataDir, "collections", "intake" )
         else:
             catalog_path = catalog_paths[0]
         os.makedirs( catalog_path, exist_ok=True )
         return catalog_path
 
     @classmethod
-    def getBasecatalog(cls, catalog_path: List[str] = None ) :
-        cats_dir = cls.getcatalogsPath()
-        cat_file_path = [ cats_dir ]
+    def getCatalogURI(cls, catalog_path: Union[str,List[str]] = None ) :
+        cats_uri = cls.getIntakeURI()
+        cat_file_path = [ cats_uri ]
         if catalog_path is not None:
             if isinstance(catalog_path, list):
                 cat_file_path.extend( catalog_path )
@@ -89,3 +94,49 @@ class Grouping:
 
     def __del__(self):
         self.close()
+
+    def addFileCollection(self, name: str, files: Union[str,List[str]], **kwargs ) -> "Grouping":
+        fileList = files if isinstance(files,list) else [ files ]
+        subGroup: Grouping =  Grouping( self._path_nodes + name, files=fileList, **kwargs )
+        subGroup.addDataSources( **kwargs )
+        self._addToCatalog( subGroup.catalog )
+        return subGroup
+
+    @property
+    def fileList(self):
+        fileList = []
+        for filesGlob in self._files:
+            fileList.extend( glob( filesGlob ) )
+        return fileList
+
+    def addDataSources( self, **kwargs ):
+        cdim = kwargs.get( "concat_dim" )
+        if cdim:
+            datasource: DataSource = self.getAggregationDataSource( cdim, **kwargs )
+            self._addToCatalog( datasource )
+        else:
+            for file in self.fileList:
+                datasource: DataSource = self.getFileDataSource( file, **kwargs )
+                self._addToCatalog( datasource )
+        self._catalog.save( self.getURI( **kwargs ) )
+
+    def getFileDataSource(self, filePath: str, **kwargs ) -> DataSource:
+        dataSource = intake.open_netcdf( filePath )
+        if dataSource is not None: self.initDataSource( dataSource, **kwargs )
+        return dataSource
+
+    def getAggregationDataSource(self, concat_dim: str, **kwargs) -> DataSource:
+        dataSource = intake.open_netcdf(self.fileList, concat_dim=concat_dim)
+        if dataSource is not None: self.initDataSource( dataSource, **kwargs )
+        return dataSource
+
+    def initDataSource(self, dataSource: DataSource, **kwargs ):
+        dataSource.discover()
+        attrs = kwargs.get("attrs", {})
+        for key, value in attrs.items(): self.setSourceAttr( dataSource, key, value)
+        dataSource.name = self.name
+        dataSource.metadata = str_dict( dataSource.metadata )
+
+    def setSourceAttr( self, dataSource: DataSource, key: str, value: str):
+        attr_value = dataSource.metadata.get(value[1:], "") if value.startswith('@') else value
+        setattr( dataSource, key, str(attr_value) )
