e4789c41a9322805c059fd7305159d328f66ca92
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-30 15:42:02 -0500

    Update Grouping api

--- collection_intake/xintake/test/adapt/scripts/merra2_ds.py
+++ collection_intake/xintake/test/adapt/scripts/merra2_ds.py
@@ -1,27 +1,18 @@
 from glob import glob
-from collection_intake.xintake.aggregation import Aggregation
-from collection_intake.xintake.collection import SourceCollection, Collection
+from collection_intake.xintake.base import Grouping, pp, str_dict
 import os, intake
 print( f"Intake drivers: {list(intake.registry)}" )
 
 collection_name = "cip_merra2_mon"
 collection_root = "/nfs4m/css/curated01/create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon"
 agg_dirs = glob( f"{collection_root}/*/*" )
-aggs = {}
+
+base_cat = Grouping.getCatalogBase()
+reanalysis_cat = base_cat.addSubGroup( "reanalysis", description="NCCS Reanalysis collections" )
+MERRA_cat = reanalysis_cat.addSubGroup( "MERRA", description="MERRA collections" )
+cip_merra2_mon_cat = MERRA_cat.addSubGroup( "cip_merra2_mon", description="MERRA2 monthly means reprocessed for CreateIP" )
 
 for agg_dir in agg_dirs:
-    agg_name = os.path.basename( agg_dir )
-    print( f"Creating aggregation {agg_name}")
-    agg_files =  f"{agg_dir}/*.nc"
-    agg = Aggregation( agg_name, collection=collection_name, files=agg_files )
-    md = agg.getMetadata()
-    agg.description = f"{md['source']}: {md['title']}"
-    agg.version = md['creation_date']
-    aggs[ agg_name ] = agg.dataSource
-
-collection = SourceCollection( collection_name, description="NASA-GMAO MERRA2 Reanalysis monthly means, reprocessed for Create-IP" )
-collection.generate( aggs = aggs )
-
-root_collection = Collection( description="NCCS data collections" )
-root_collection.generate(  )
+    print( f"Adding cip_merra2_mon aggregation for data path {agg_dir}")
+    cip_merra2_mon_cat.addDataSources( f"{agg_dir}/*.nc", driver="netcdf", concat_dim="time" )
 
