34dfd85c21ad1c30c6af42010df30447d1813c81
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-12-31 14:42:48 -0500

    Add catalogs test

--- collection_intake/xintake/test/files_cat_test.py
+++ collection_intake/xintake/test/files_cat_test.py
@@ -3,5 +3,6 @@ from intake.catalog.local import YAMLFilesCatalog
 from typing import Dict
 
 cat_items = "/att/pubrepo/ILAB/data/collections/intake/reanalysis/MERRA/cip_merra2_mon/*.yaml"
-catalogs: YAMLFilesCatalog = intake.open_catalog(cat_items, driver="yaml_files_cat")
+catalogs: YAMLFilesCatalog = intake.open_catalog(cat_items, driver="yaml_files_cat", description="MERRA2 monthly means reprocessed for CreateIP")
+catalogs.name = 'cip_merra2_mon'
 catalogs.save( "/tmp/files_cat_test.yaml")
