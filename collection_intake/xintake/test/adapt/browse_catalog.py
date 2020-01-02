0c60080c0aa5a060b71aee7d6c9bb012685d79ac
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-02 18:01:06 -0500

    Add browse test

--- collection_intake/xintake/test/adapt/browse_catalog.py
+++ collection_intake/xintake/test/adapt/browse_catalog.py
@@ -11,4 +11,8 @@ print( image.discover() )
 
 ang_rdn_v2r2: Catalog = collections.getCatalog( 'image/ABoVE/ORNL_AVIRIS_NG/ang_rdn_v2r2')
 
-print( list(ang_rdn_v2r2) )
+
+
+data = ang_rdn_v2r2['ang_rdn_v2r2-99-2018-07-22_23-00-37'].read()
+
+print( data.__class__.__name__ )
