0f198811250a2352db6c70c5f5f4f87e0c1a519f
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-02 18:14:21 -0500

    Add browse test

--- collection_intake/xintake/test/adapt/browse_catalog.py
+++ collection_intake/xintake/test/adapt/browse_catalog.py
@@ -2,6 +2,7 @@ import intake
 from intake.catalog.local import YAMLFileCatalog, Catalog, LocalCatalogEntry
 from collection_intake.xintake.catalog import CatalogNode
 from collection_intake.xintake.manager import collections
+from intake_xarray.raster import RasterIOSource
 def cn( x ): return x.__class__.__name__
 def pcn( x ): print( x.__class__.__name__ )
 
@@ -11,10 +12,8 @@ ang_rdn_v2r2: Catalog = collections.getCatalog( cat_path )
 
 print( f'Result: {ang_rdn_v2r2.discover()}'  )
 
-data_source =  ang_rdn_v2r2['ang_rdn_v2r2-99-2018-07-22_23-00-37'].get()
+data_source: RasterIOSource =  ang_rdn_v2r2['ang_rdn_v2r2-99-2018-07-22_23-00-37'].get()
 
-print( f'Source Node: {cn(data_source)}'  )
+dask_data_source = data_source.to_dask()
+print( f'dask_data_source: {cn(dask_data_source)}'  )
 
-data = data_source.read()
-
-print( data.__class__.__name__ )
