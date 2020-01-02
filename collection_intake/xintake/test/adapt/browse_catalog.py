72417bcece52740fb428930606471e091458cd08
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-02 18:07:38 -0500

    Add browse test

--- collection_intake/xintake/test/adapt/browse_catalog.py
+++ collection_intake/xintake/test/adapt/browse_catalog.py
@@ -2,17 +2,19 @@ import intake
 from intake.catalog.local import YAMLFileCatalog, Catalog, LocalCatalogEntry
 from collection_intake.xintake.catalog import CatalogNode
 from collection_intake.xintake.manager import collections
+def cn( x ): return x.__class__.__name__
+def pcn( x ): print( x.__class__.__name__ )
 
-image_entry: LocalCatalogEntry = collections.catalog.image
-print( image_entry.describe() )
+cat_path = 'image/ABoVE/ORNL_AVIRIS_NG/ang_rdn_v2r2'
+print( f'Reading {cat_path}' )
+ang_rdn_v2r2: Catalog = collections.getCatalog( cat_path )
 
-image: YAMLFileCatalog = image_entry.get()
-print( image.discover() )
+print( f'Result: {ang_rdn_v2r2.discover()}'  )
 
-ang_rdn_v2r2: Catalog = collections.getCatalog( 'image/ABoVE/ORNL_AVIRIS_NG/ang_rdn_v2r2')
+data_source =  ang_rdn_v2r2['ang_rdn_v2r2-99-2018-07-22_23-00-37']
 
+print( f'Source Node: {cn(data_source)}'  )
 
-
-data = ang_rdn_v2r2['ang_rdn_v2r2-99-2018-07-22_23-00-37'].read()
+data = data_source.read()
 
 print( data.__class__.__name__ )
