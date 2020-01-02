0206b3afc0590d34e867d4f2136ded45d562e75c
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-02 18:26:14 -0500

    Add browse test

--- collection_intake/xintake/test/adapt/generate_aviris_catalog.py
+++ collection_intake/xintake/test/adapt/generate_aviris_catalog.py
@@ -14,14 +14,14 @@ def get_source_name( collection: str, index: int, fname: str ):
 collection_name = "ORNL_ABoVE_Airborne_AVIRIS_NG"
 collection_root = "/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG"
 aggregations = dict( ang_rdn_v2r2 = f"{collection_root}/data/ang*/ang*_rdn_v2r2/*_img" )
-sub_collections = []
+chunks=dict( band=10 )
 
 base_cat: CatalogNode = CatalogNode.getCatalogBase()
 image_cat: CatalogNode = base_cat.addCatalogNode( "image", description="Remote sensing image collections" )
 ABoVE_cat: CatalogNode = image_cat.addCatalogNode( "ABoVE", description="ABoVE Project data collections" )
 ORNL_AVIRIS_NG_cat: CatalogNode = ABoVE_cat.addCatalogNode( "ORNL_AVIRIS_NG", description="ORNL ABoVE Airborne AVIRIS NG Collections" )
 ang_rdn_v2r2: DataCollectionGenerator = ORNL_AVIRIS_NG_cat.addDataCollection( "ang_rdn_v2r2", description="ang_rdn_v2r2 collection", metadata={} )
-ang_rdn_v2r2.addFileCollection( f"{collection_root}/data/ang*/ang*_rdn_v2r2/*_img", get_name=get_source_name, driver="rasterio", chunks={} )
+ang_rdn_v2r2.addFileCollection( f"{collection_root}/data/ang*/ang*_rdn_v2r2/*_img", get_name=get_source_name, driver="rasterio", chunks=chunks )
 
 
 
