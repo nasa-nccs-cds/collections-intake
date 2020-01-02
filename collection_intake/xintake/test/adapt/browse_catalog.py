5b54a595d71c998305926c893af2ec40bd9debe9
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-02 17:11:59 -0500

    Add browse test

--- collection_intake/xintake/test/adapt/browse_catalog.py
+++ collection_intake/xintake/test/adapt/browse_catalog.py
@@ -1,14 +1,16 @@
 import intake
-from intake.catalog.local import YAMLFileCatalog, YAMLFilesCatalog, Catalog, LocalCatalogEntry
+from intake.catalog.local import YAMLFileCatalog, Catalog, LocalCatalogEntry
 from collection_intake.xintake.catalog import CatalogNode
 from collection_intake.xintake.manager import collections
 
 image_entry: LocalCatalogEntry = collections.catalog.image
-image: Catalog = image_entry.get()
-
 print( image_entry.describe() )
 
+image: YAMLFileCatalog = image_entry.get()
+image.discover()
+
 print( "Elements:" )
-for item in image.items():
-    print( item.__class__.__name__ )
-    print( item )
+print( list( dict( image.items() ).keys() ) )
+
+above_entry = image.ABoVE
+print( above_entry.describe() )
