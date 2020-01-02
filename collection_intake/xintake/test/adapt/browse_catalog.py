1bfe923d6e3a18755c9374813d1c0dd149ee9b23
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-02 17:59:07 -0500

    Add browse test

--- collection_intake/xintake/test/adapt/browse_catalog.py
+++ collection_intake/xintake/test/adapt/browse_catalog.py
@@ -9,5 +9,6 @@ print( image_entry.describe() )
 image: YAMLFileCatalog = image_entry.get()
 print( image.discover() )
 
-ang_rdn_v2r2 = collections.getCatalog( 'image/ABoVE/ORNL_AVIRIS_NG/ang_rdn_v2r2')
-print( 'ang_rdn_v2r2: ' + ang_rdn_v2r2.__class__.__name__ )
+ang_rdn_v2r2: Catalog = collections.getCatalog( 'image/ABoVE/ORNL_AVIRIS_NG/ang_rdn_v2r2')
+
+print( list(ang_rdn_v2r2) )
