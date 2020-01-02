7abbad033856e1397ad132af68fdc61a56944cdd
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-02 16:50:02 -0500

    Refactor API

--- collection_intake/xintake/test/adapt/browse_catalog.py
+++ collection_intake/xintake/test/adapt/browse_catalog.py
@@ -3,9 +3,9 @@ from intake.catalog.local import YAMLFileCatalog, YAMLFilesCatalog, Catalog
 from collection_intake.xintake.catalog import CatalogNode
 from collection_intake.xintake.manager import collections
 
-images: Catalog = collections.catalog.image
-print( images )
+ABoVE: Catalog = collections.catalog.image.ABoVE
+print( ABoVE )
 
 print( "Elements:" )
-for item in images.items():
+for item in ABoVE.items():
     print( item )
