c6f9dff1966a68d4862aae9975f9d1cf2a4af443
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-03 13:10:08 -0500

    Add merra2 script

--- collection_intake/xintake/test/adapt/merra2_ds.py
+++ collection_intake/xintake/test/adapt/merra2_ds.py
@@ -1,5 +1,6 @@
 from glob import glob
-from collection_intake.xintake.base import IntakeNode, pp, str_dict
+from collection_intake.xintake.catalog import CatalogNode
+from collection_intake.xintake.collection import DataCollectionGenerator
 import os, intake
 print( f"Intake drivers: {list(intake.registry)}" )
 
@@ -7,13 +8,13 @@ collection_name = "cip_merra2_mon"
 collection_root = "/nfs4m/css/curated01/create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon"
 agg_dirs = glob( f"{collection_root}/*/*" )
 
-base_cat = IntakeNode.getCatalogBase()
-reanalysis_cat = base_cat.addSubGroup( "reanalysis", description="NCCS Reanalysis collections" )
-MERRA_cat = reanalysis_cat.addSubGroup( "MERRA", description="MERRA collections" )
-cip_merra2_mon_cat = MERRA_cat.addSubGroup( "cip_merra2_mon", description="MERRA2 monthly means reprocessed for CreateIP" )
+base_cat: CatalogNode = CatalogNode.getCatalogBase()
+reanalysis_cat = base_cat.addCatalogNode( "reanalysis", description="NCCS Reanalysis collections" )
+MERRA_cat = reanalysis_cat.addCatalogNode( "MERRA", description="MERRA collections" )
+cip_merra2_mon: DataCollectionGenerator = MERRA_cat.addDataCollection( "cip_merra2_mon", description="MERRA2 monthly means reprocessed for CreateIP" )
 
 for agg_dir in agg_dirs:
     print( f"Adding aggregation data source to collection cip_merra2_mon for data path {agg_dir}")
     dsname = os.path.basename(agg_dir)
-    cip_merra2_mon_cat.addDataSource( dsname, f"{agg_dir}/*.nc", driver="netcdf", concat_dim="time", chunks={} )
+    cip_merra2_mon.addAggregation( dsname, f"{agg_dir}/*.nc", driver="netcdf", concat_dim="time", chunks={} )
 
