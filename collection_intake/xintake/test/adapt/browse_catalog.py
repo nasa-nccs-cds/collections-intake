89f3b487ecca0d8782b8c98b60c62107aa2d2787
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-02 19:54:34 -0500

    Add browse test

--- collection_intake/xintake/test/adapt/browse_catalog.py
+++ collection_intake/xintake/test/adapt/browse_catalog.py
@@ -2,26 +2,31 @@ import intake, time
 from intake.catalog.local import YAMLFileCatalog, Catalog, LocalCatalogEntry
 from collection_intake.xintake.catalog import CatalogNode
 from collection_intake.xintake.manager import collections
+from geoproc.cluster.manager import ClusterManager
 import xarray as xa
 from intake_xarray.raster import RasterIOSource
 def cn( x ): return x.__class__.__name__
 def pcn( x ): print( x.__class__.__name__ )
+cluster_parameters = { "log.scheduler.metrics": False, 'type': 'slurm' }
 
-cat_path = 'image/ABoVE/ORNL_AVIRIS_NG/ang_rdn_v2r2'
-print( f'Reading {cat_path}' )
-ang_rdn_v2r2: Catalog = collections.getCatalog( cat_path )
+with ClusterManager( cluster_parameters ) as clusterMgr:
 
-print( f'Result: {ang_rdn_v2r2.discover()}'  )
+    cat_path = 'image/ABoVE/ORNL_AVIRIS_NG/ang_rdn_v2r2'
+    print( f'Reading {cat_path}' )
+    ang_rdn_v2r2: Catalog = collections.getCatalog( cat_path )
 
-data_source: RasterIOSource =  ang_rdn_v2r2['ang_rdn_v2r2-99-2018-07-22_23-00-37'].get()
+    print( f'Result: {ang_rdn_v2r2.discover()}'  )
 
-dask_data_source: xa.DataArray = data_source.to_dask()
-print( f'dask_data_source, shape: {dask_data_source.shape}, dims: {dask_data_source.dims}, chunks: {dask_data_source.chunks}'  )
+    data_source: RasterIOSource =  ang_rdn_v2r2['ang_rdn_v2r2-99-2018-07-22_23-00-37'].get()
 
-t0 = time.time()
-dask_data_source.persist()
-t1 = time.time()
+    dask_data_source: xa.DataArray = data_source.to_dask()
+    print( f'dask_data_source, shape: {dask_data_source.shape}, dims: {dask_data_source.dims}, chunks: {dask_data_source.chunks}'  )
 
-print( f'Completed reading array in {t1-t0} secs'  )
+    t0 = time.time()
+    dask_data_source.persist()
+    t1 = time.time()
+    dt = t1-t0
+
+    print( f'Completed reading array in {int(dt)} secs ( {dt/60.0} min )'  )
 
 
