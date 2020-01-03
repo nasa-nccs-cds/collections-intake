b64c8d795389a58774d0489af11b34eeabe8cf5c
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-03 15:49:23 -0500

    Add merra2 script

--- collection_intake/xintake/scripts/adapt/merra2.py
+++ collection_intake/xintake/scripts/adapt/merra2.py
@@ -8,7 +8,7 @@ import multiprocessing as mp
 cBase: CatalogNode = CatalogNode.getCatalogBase()
 cReanalysis = cBase.addCatalogNode( "reanalysis", description="NCCS Reanalysis collections" )
 cMERRA2 = cReanalysis.addCatalogNode( "MERRA2", description="MERRA2 from NASA Global Modeling and Assimilation Office" )
-cRaw: DataCollectionGenerator = cMERRA2.addDataCollection( "raw", description="1-Hourly global: 0.5 x 0.625 degree resolution " )
+cHourly: DataCollectionGenerator = cMERRA2.addDataCollection( "hourly", description="1-Hourly global: 0.5 x 0.625 degree resolution " )
 
 collection_root = "/att/pubrepo/MERRA2/local/"
 agg_dirs = glob( f"{collection_root}/*" )
@@ -16,7 +16,7 @@ agg_dirs = glob( f"{collection_root}/*" )
 def createAggregation( agg_dir: str ):
     print( f"Adding aggregation data source to collection MERRA2-raw for data path {agg_dir}")
     dsname = os.path.basename(agg_dir)
-    cRaw.addAggregation( dsname, glob(f"{agg_dir}/*/*/*.nc4"), driver="netcdf", concat_dim="time", chunks={} )
+    cHourly.addAggregation( dsname, glob(f"{agg_dir}/*/*/*.nc4"), driver="netcdf", concat_dim="time", chunks={} )
 
 nproc = 2*mp.cpu_count()
 chunksize = math.ceil( len(agg_dirs) / nproc )
