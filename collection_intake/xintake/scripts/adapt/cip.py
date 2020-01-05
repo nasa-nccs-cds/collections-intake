858233523d89f66db19ba72f949b9533ebb8a1dd
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-05 14:40:20 -0500

    Add cip script

--- collection_intake/xintake/scripts/adapt/cip.py
+++ collection_intake/xintake/scripts/adapt/cip.py
@@ -17,6 +17,7 @@ ecmwf_file_path1 = "ECMWF/IFS-Cy31r2/ERA-Interim"
 cECMWF = cCip.addCatalogNodes( ecmwf_file_path1  )
 for dset_dir in glob( f"{collection_root}/{ecmwf_file_path1}/*" ):
     dset_name = path.basename(dset_dir)
+    print( f"Adding dataset {dset_name} for path {dset_dir}")
     dset_node: DataCollectionGenerator = cECMWF.addDataCollection( dset_name  )
     for col_path in glob( "dset_dir/*" ):
         col_name  = path.basename(col_path)
