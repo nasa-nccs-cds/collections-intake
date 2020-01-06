78442a9cb6c9530dc43ac36f26ff150084f03f62
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-06 14:20:41 -0500

    Add file test script

--- collection_intake/xintake/scripts/adapt/test_files.py
+++ collection_intake/xintake/scripts/adapt/test_files.py
@@ -2,42 +2,37 @@ import os, xarray, time, math
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
 from multiprocessing import Pool
 import multiprocessing as mp
+from glob import glob
 
-base_dir = "/nfs4m/css/curated01/merra2"
+base_dirs = glob("/nfs4m/css/curated01/merra2/data/*")
+bad_files_name = f"/tmp/bad_files-merra2.csv"
 suffix = ".nc4"
-lines = []
+
 file_locations = []
+lines = []
 
-print( f"  Walking directory structure" )
-t0 = time.time()
-for root, dirs, files in os.walk( base_dir ):
-   if len( files ) > 0:
-       file_locations.append( dict( dir=root, files=files ) )
-print( f"\n  Completed walking directory structure in {(time.time()-t0)} seconds\n" )
-
-def test_files( file_locations: Dict ):
-   root = file_locations["root"]
-   files = file_locations["files"]
-   print( f"     -- Scanning dir {root}, nfiles: {len(files)}" )
-   for fname in files:
-       if fname.endswith(suffix):
-           file_path = os.path.join(root, fname)
-           try:
-               ds = xarray.open_dataset(file_path)
-           except Exception as err:
-               print(f"** Found bad file {file_path}, err: {err}")
-               lines.append( f"{file_path}, {err}" )
+def test_files( base_dir: str ):
+    for root, dirs, files in os.walk( base_dir ):
+        if len(files ) > 0:
+           print(".", end =" ")
+           for fname in files:
+               if fname.endswith(suffix):
+                   file_path = os.path.join(root, fname)
+                   try:
+                       ds = xarray.open_dataset(file_path)
+                   except Exception as err:
+                       print(f"\n ** Found bad file {file_path}, err: {err}")
+                       lines.append( f"{file_path}, {err}" )
 
 
 t0 = time.time()
 nproc = 2*mp.cpu_count()
 chunksize = math.ceil( len(file_locations) / nproc )
 with Pool(processes=nproc) as pool:
-    pool.map( test_files, file_locations, chunksize )
+    pool.map( base_dirs, test_files, chunksize )
 
 print( f"Completed test_files in {(time.time()-t0)/60.0} minutes using {nproc} processes" )
 
-bad_files_name = f"/tmp/bad_files-{os.path.basename(base_dir)}.csv"
 bad_files = open(bad_files_name, "w")
 bad_files.writelines(lines)
 bad_files.close()
