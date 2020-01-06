6d6df5f7846fd7f7e7e4c52b7bca6ebd917dc239
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-06 14:06:09 -0500

    Add file test script

--- collection_intake/xintake/scripts/adapt/test_files.py
+++ collection_intake/xintake/scripts/adapt/test_files.py
@@ -6,13 +6,17 @@ import multiprocessing as mp
 base_dir = "/nfs4m/css/curated01/merra2"
 suffix = ".nc4"
 lines = []
-files = []
+file_locations = []
 
+t0 = time.time()
 for root, dirs, files in os.walk( base_dir ):
    if len( files ) > 0:
-       files.append( dict( dir=root, files=files ) )
+       file_locations.append( dict( dir=root, files=files ) )
+print( f"\n  Completed walking directory structure in {(time.time()-t0)} seconds\n" )
 
-def test_files( root: str, files: List[str]):
+def test_files( file_locations: Dict ):
+   root = file_locations["root"]
+   files = file_locations["files"]
    print( f"     -- Scanning dir {root}, nfiles: {len(files)}" )
    for fname in files:
        if fname.endswith(suffix):
@@ -26,9 +30,9 @@ def test_files( root: str, files: List[str]):
 
 t0 = time.time()
 nproc = 2*mp.cpu_count()
-chunksize = math.ceil( len(files) / nproc )
+chunksize = math.ceil( len(file_locations) / nproc )
 with Pool(processes=nproc) as pool:
-    pool.map( test_files, files, chunksize )
+    pool.map( test_files, file_locations, chunksize )
 
 print( f"Completed test_files in {(time.time()-t0)/60.0} minutes using {nproc} processes" )
 
