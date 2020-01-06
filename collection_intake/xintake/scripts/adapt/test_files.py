6c22eadbbffef8f6b5e837813c4f1719da177fb9
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-06 13:50:47 -0500

    Add file test script

--- collection_intake/xintake/scripts/adapt/test_files.py
+++ collection_intake/xintake/scripts/adapt/test_files.py
@@ -1,9 +1,18 @@
-import os, xarray
+import os, xarray, time, math
+from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
+from multiprocessing import Pool
+import multiprocessing as mp
+
 base_dir = "/nfs4m/css/curated01/merra2"
 suffix = ".nc4"
-
 lines = []
+files = []
+
 for root, dirs, files in os.walk( base_dir ):
+   if len( files ) > 0:
+       files.append( dict( dir=root, files=files ) )
+
+def test_files( root: str, files: List[str]):
    print( f"     -- Scanning dir {root}, nfiles: {len(files)}" )
    for fname in files:
        if fname.endswith(suffix):
@@ -13,6 +22,15 @@ for root, dirs, files in os.walk( base_dir ):
            except Exception as err:
                print(f"** Found bad file {file_path}, err: {err}")
                lines.append( f"{file_path}, {err}" )
+
+
+t0 = time.time()
+nproc = 2*mp.cpu_count()
+chunksize = math.ceil( len(files) / nproc )
+with Pool(processes=nproc) as pool:
+    pool.map( test_files, files, chunksize )
+
+print( f"Completed test_files in {(time.time()-t0)/60.0} minutes using {nproc} processes" )
 
 bad_files_name = f"/tmp/bad_files-{os.path.basename(base_dir)}.csv"
 bad_files = open(bad_files_name, "w")
