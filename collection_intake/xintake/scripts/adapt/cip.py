ebc3e1a21b04336ed0349bcd8a2e32a60a2b7b0c
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-05 20:31:35 -0500

    Add cip script

--- collection_intake/xintake/scripts/adapt/cip.py
+++ collection_intake/xintake/scripts/adapt/cip.py
@@ -1,7 +1,10 @@
 from glob import glob
+from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
 from collection_intake.xintake.catalog import CatalogNode
 from collection_intake.xintake.collection import DataCollectionGenerator
-import os, intake
+import os, intake, math, time
+from multiprocessing import Pool
+import multiprocessing as mp
 from os import path
 
 print( f"Intake drivers: {list(intake.registry)}" )
@@ -13,16 +16,70 @@ base_cat: CatalogNode = CatalogNode.getCatalogBase()
 cReanalysis = base_cat.addCatalogNode( "reanalysis", description="NCCS Reanalysis collections" )
 cCip = cReanalysis.addCatalogNode( "createIP", description="Reprocessed reanalyses for CreateIP" )
 
-ecmwf_file_path1 = "ECMWF/IFS-Cy31r2/ERA-Interim"
-cECMWF = cCip.addCatalogNodes( ecmwf_file_path1  )
-for dset_dir in glob( f"{collection_root}/{ecmwf_file_path1}/*" ):
-    dset_name = path.basename(dset_dir)
-    print( f"\n   ****> Adding dataset {dset_name} for path {dset_dir}")
-    dset_node: DataCollectionGenerator = cECMWF.addDataCollection( dset_name  )
-    for col_path in glob( f"{dset_dir}/*" ):
-        col_name  = path.basename(col_path)
-        for agg_path in glob(  f"{col_path}/*" ):
-            agg_name  = f"{col_name}-{path.basename(agg_path)}"
-            dset_node.addAggregation( agg_name, f"{agg_path}/*.nc", driver="netcdf", concat_dim="time", chunks={})
+def addCatNodes( baseNode: CatalogNode, curdir: str ):
+    if len( glob(f"{curdir}/*.nc") ) > 0 :
+        data_collection: DataCollectionGenerator = baseNode.addDataCollection( path.basename(curdir) )
+        data_collection.addAggregation( 'netcdf_files', f"{curdir}/*.nc", driver="netcdf", concat_dim="time", chunks={})
+    else:
+        node_name = path.basename( curdir )
+        current_node = baseNode.addCatalogNodes(node_name)
+        for subDir in glob(f"{curdir}/*"):
+            if os.path.isdir( subDir ):
+                addCatNodes( current_node, path.join( curdir, subDir ) )
 
+for collDir in glob(f"{collection_root}/*"):
+    addCatNodes( cCip, collDir )
 
+#
+# def addCatNode(baseNode: CatalogNode, curdir: str):
+#     if len(glob(f"{curdir}/*.nc")) > 0:
+#         data_collection: DataCollectionGenerator = baseNode.addDataCollection(path.basename(curdir))
+#         data_collection.addAggregation('files', f"{curdir}/*.nc", driver="netcdf", concat_dim="time", chunks={})
+#
+#     subDirs = [sdir for sdir in os.listdir(curdir) if path.isdir(path.join(curdir, sdir))]
+#     if len(set(subDirs).intersection(["day", "mon", "mth", "6hr"])) > 0:
+#         for timeResDir in subDirs:
+#             data_collection: DataCollectionGenerator = baseNode.addDataCollection(path.basename(curdir))
+#         for sdir in subDirs:
+#             data_collection.addAggregation(sdir, f"{curdir}/{sdir}/**/*.nc", driver="netcdf", concat_dim="time", chunks={})
+#     else:
+#         node_name = path.basename(curdir)
+#         current_node = baseNode.addCatalogNodes(node_name)
+#         for subDir in subDirs:
+#             addCatNode(current_node, path.join(curdir, subDir))
+#
+#
+# base_name = "ECMWF"
+# for model_dir in glob( f"{collection_root}/{base_name}/*" ):
+#     model_name = path.basename( model_dir )
+#     model_node = cCip.addCatalogNodes( model_name )
+#     for model_dir in glob(f"{collection_root}/{base_name}/*"):
+#         model_name = path.basename(model_dir)
+#         model_node = cCip.addCatalogNodes(model_name)
+#
+# base_file_path = "ECMWF/IFS-Cy31r2/ERA-Interim"
+# cECMWF = cCip.addCatalogNodes( ecmwf_file_path1  )
+#
+# agg_op_params = []
+# for dset_dir in glob( f"{collection_root}/{ecmwf_file_path1}/*" ):
+#     dset_name = path.basename(dset_dir)
+#     print( f"\n   ****> Adding dataset {dset_name} for path {dset_dir}")
+#     dset_node: DataCollectionGenerator = cECMWF.addDataCollection( dset_name  )
+#     for col_path in glob( f"{dset_dir}/*" ):
+#         col_name  = path.basename(col_path)
+#         for agg_path in glob(  f"{col_path}/*" ):
+#             agg_name  = f"{col_name}-{path.basename(agg_path)}"
+#             agg_op_params.append( dict( node=dset_node, name=agg_name, files=f"{agg_path}/*.nc" ) )
+#
+# def add_agg( params: Dict ):
+#     params['node'].addAggregation( params['name'], params['files'], driver="netcdf", concat_dim="time", chunks={})
+#
+# t0 = time.time()
+# nproc = 2*mp.cpu_count()
+# chunksize = math.ceil( len(agg_op_params) / nproc )
+# with Pool(processes=nproc) as pool:
+#     pool.map( add_agg, agg_op_params, chunksize )
+#
+# print( f"Completed creating catalog in {(time.time()-t0)/60.0} minutes using {nproc} processes" )
+#
+#
