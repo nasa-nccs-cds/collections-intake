bf2d7e2dac0edec013c25f63726afe37fa8c847c
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-06 14:54:08 -0500

    Update show_GUI

--- collection_intake/xintake/scripts/adapt/test/show_gui.py
+++ collection_intake/xintake/scripts/adapt/test/show_gui.py
@@ -3,6 +3,6 @@ from collection_intake.xintake.catalog import CatalogNode
 
 base_catalog: CatalogNode = CatalogNode.getCatalogBase()
 print( f"Starting intake gui with base catalog: {base_catalog}")
-gui = gui.GUI( base_catalog )
+gui = gui.GUI( [base_catalog.catalog] )
 
 gui.show( )
