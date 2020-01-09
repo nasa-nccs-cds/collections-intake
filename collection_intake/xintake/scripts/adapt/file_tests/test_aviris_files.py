d8acd512e2674efb11181b9be4dc23a6a2965bce
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-09 17:07:17 -0500

    add file tester

--- collection_intake/xintake/scripts/adapt/file_tests/test_aviris_files.py
+++ collection_intake/xintake/scripts/adapt/file_tests/test_aviris_files.py
@@ -2,7 +2,7 @@ import os, xarray, time, math
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
 from collection_intake.util.fileTest import FileTester
 
-base_dirs = "/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NGdata/*/*"
+base_dirs = "/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG/data/*"
 suffix = "_img"
 engine = "rasterio"
 
