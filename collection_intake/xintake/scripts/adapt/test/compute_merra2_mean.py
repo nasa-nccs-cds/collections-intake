db6105d0b85f2588005a32ffa67aa95fcd0b4ba0
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-02-04 16:55:10 -0500

    add merra2 read test

--- collection_intake/xintake/scripts/adapt/test/compute_merra2_mean.py
+++ collection_intake/xintake/scripts/adapt/test/compute_merra2_mean.py
@@ -9,7 +9,7 @@ print( f"Intake drivers: {list(intake.registry)}" )
 # chunks = dict( time=24 )
 
 with ClusterManager( cluster_parameters ) as clusterMgr:
-    files_path = "/att/pubrepo/MERRA2/local/M2T1NXLND.5.12.4/*/*/*.nc4"
+    files_path = "/att/pubrepo/MERRA2/local/M2T1NXLND.5.12.4/2000/*/*.nc4"
     files = glob( files_path)
     t0 = time.time()
     print( f"Opening {len(files)} files from {files_path} using ClusterManager")
